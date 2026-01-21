f2f8d33f3915fe7a5493a35592d3dc0c4acaf988
attack-surface1 <attacksurface100@gmail.com>
2026-01-21 05:23:57 -0500

    File Clean Up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const LinkItem = (props) => {
-  return <Link style={{fontSize: "50px", backgroundColor: "lightgrey", padding: "8px 12px", margin: "0 10px", borderRadius: "6px"}} to={props.to}>{props.text}</Link>
+  return <Link style={{fontSize: "60px", margin: "0 10px"}} to={props.to}>{props.text}</Link>
 } 
 
 const Home = () => {
   return (
-    <div style={{display: "flex", justifyContent: "space-between", width: "600px", margin: "90px auto"}}>
+    <div style={{display: "flex", justifyContent: "space-between", width: "600px", margin: "200px auto"}}>
       <LinkItem to="/lab-one/1" text="Lab 1"/>
       <LinkItem to="/lab-two" text="Lab 2"/>
     </div>
